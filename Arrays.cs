699344332985f1a66c08160a084b7d19945a6e63
craigtupac-96 <Siberia1#>
2019-07-30 13:36:52 +0100

    Adding a simple array example (to be expanded upon)

---
+++ Arrays.cs
@@ -0,0 +1,41 @@
+﻿/*
+    Author: Craig Lawlor
+    Description: A program to explore arrays
+*/
+using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // cw tab - shortcut
+            Console.Write("How many elements? ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[] numbersArray = new int[size];
+
+            // input for array
+            for ( int i = 0; i < size; i++ )
+            {
+                Console.WriteLine("Enter a number");
+                numbersArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // output for array
+            for ( int i = 0; i < size; i++ )
+            {
+                Console.WriteLine((i + 1) + ": " + numbersArray[i]);
+            }
+
+            Console.WriteLine("{0}Output with a foreach", Environment.NewLine);
+            foreach (int i in numbersArray)
+            {
+                Console.WriteLine(i);
+            }
+
+            // add a multidimensional array here
+        }
+    }
+}
